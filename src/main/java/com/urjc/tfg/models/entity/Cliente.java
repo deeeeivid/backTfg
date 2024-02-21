f48af3db10f407f215b74e325ccf785f15bec4ce
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-02-21 20:48:29 +0100

    añadir entity Cliente

---
+++ src/main/java/com/urjc/tfg/models/entity/Cliente.java
@@ -0,0 +1,34 @@
+package com.urjc.tfg.models.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+}
