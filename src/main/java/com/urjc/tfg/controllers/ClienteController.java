5e6f9c821d24aa307eb2dd9ce3fbce9cbbe3e724
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-02-21 22:38:31 +0100

    añadir CrossOrigin para conectar con front

--- src/main/java/com/urjc/tfg/controllers/ClienteController.java
+++ src/main/java/com/urjc/tfg/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.urjc.tfg.controllers;
 import com.urjc.tfg.models.entity.Cliente;
 import com.urjc.tfg.models.services.IClienteService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @AllArgsConstructor
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteController {
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('David', 'Muñoz', 'david@hotmail.com', '2024-02-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Ivan', 'Pérez', 'ivan@hotmail.com', '2024-02-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Victor', 'Aguilar', 'victor@hotmail.com', '2024-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Raquel', 'Trino', 'raquel@hotmail.com', '2024-02-18');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Maria', 'Muñoz', 'maria@hotmail.com', '2024-02-15');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Pedro', 'López', 'pedro@hotmail.com', '2024-02-10');
