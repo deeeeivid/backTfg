e90e3a259218398aa19b5ad85133566187ea5815
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-04-27 20:30:30 +0200

    eliminar Factura

--- src/main/java/com/urjc/tfg/controllers/FacturaController.java
+++ src/main/java/com/urjc/tfg/controllers/FacturaController.java
@@ -4,6 +4,7 @@ import com.urjc.tfg.models.entity.Factura;
 import com.urjc.tfg.services.IFacturaService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,12 @@ public class FacturaController {
     @GetMapping("/facturas/{id}")
     public ResponseEntity<Factura> show(@PathVariable Long id) {
         return ResponseEntity.ok(facturaService.findById(id));
+    }
+
+    @DeleteMapping("/facturas/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        facturaService.deleteById(id);
     }
 
 }
