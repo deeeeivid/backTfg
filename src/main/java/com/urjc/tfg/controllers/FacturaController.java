02f26d996e6f1fea342e8e35157ad2a311dce9bb
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-05-02 18:03:07 +0200

    spring security mas recursion editar

--- src/main/java/com/urjc/tfg/controllers/FacturaController.java
+++ src/main/java/com/urjc/tfg/controllers/FacturaController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,23 +25,27 @@ public class FacturaController {
     private IProductoService productoService;
 
     @GetMapping("/facturas/{id}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER')")
     public ResponseEntity<Factura> show(@PathVariable Long id) {
         return ResponseEntity.ok(facturaService.findById(id));
     }
 
     @PostMapping("/facturas")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     public Factura crear(@RequestBody Factura factura) {
         return facturaService.save(factura);
     }
 
     @DeleteMapping("/facturas/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         facturaService.deleteById(id);
     }
 
     @GetMapping("/facturas/filtrar-productos/{nombre}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.OK)
     public List<Producto> filtrarProductos(@PathVariable String nombre) {
         return productoService.findByNombre(nombre);
